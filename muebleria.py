80e3640005b048e82957125c15f0288156e93b7e
cara0000 <lucas.caracciolo654@gmail.com>
2023-07-01 20:58:08 -0300

    'progreso vista ventas'

--- muebleria.py
+++ muebleria.py
@@ -47,7 +47,7 @@ if ('muebleria0000',) not in bds:
 
     mi_cursor.execute("CREATE TABLE muebles (id int primary key auto_increment, designacion VARCHAR(50), precio_unitario int, stock int)")
 
-    insertar_datos = "INSERT INTO muebles (designacion, precio_unitario, stock) VALUES (%s, %s, %s)"
+    insertar_datos = "INSERT INTO muebles (designacion, precio_unitario, stock) VALUES (%s, %s, %s)" #agregar descripcion
     valores = [
     ('banqueta_individual', 110, 0),
     ('esquinero', 130, 0),
@@ -64,36 +64,82 @@ if ('muebleria0000',) not in bds:
     ('gazebo_doble', 1700, 0)
     ]
     mi_cursor.executemany(insertar_datos, valores)
+    print(mi_cursor.rowcount, "registros insertados en la tabla 'muebles'.")
+
+    mi_cursor.execute("CREATE TABLE clientes (id INT PRIMARY KEY AUTO_INCREMENT, nombre VARCHAR(50), apellido VARCHAR(50))")
+
+    mi_cursor.execute("CREATE TABLE ventas (id INT PRIMARY KEY AUTO_INCREMENT, id_cliente INT, total FLOAT, fecha DATE, FOREIGN KEY (id_cliente) REFERENCES clientes(id))")
+
+    mi_cursor.execute("CREATE TABLE detalles_ventas (id INT PRIMARY KEY AUTO_INCREMENT, id_venta INT, id_mueble INT, FOREIGN KEY (id_mueble) REFERENCES muebles(id), FOREIGN KEY (id_venta) REFERENCES ventas(id))")
+
     base_datos.commit()
-    print(mi_cursor.rowcount, "registros insertados.")
 else:
     mi_cursor.execute("use muebleria0000")
     print("BD 'muebleria0000' designada.")
 
 #alter table change campoviejo camponuevo varchar(50) --> para modificar creo
+#clases: interfaz para hacer mas manejable el programa, no pasa naranja si no tenes un __init__
 
-#clases
-class Muebleria:
+from abc import ABC, abstractmethod
+
+class Persona(ABC):
     def __init__(self):
-        self.empleados=[]
-        self.muebles=[]
-
-    def set_empleados(self,empleados):
-        self.empleados= empleados
-
-    def get_empleados(self):
-        return self.empleados
+        self.nombre= ""
+        self.apellido= ""
     
-    def set_muebles(self,muebles):
-        self.muebles= muebles
+    @abstractmethod
+    def set_nombre(self):
+        pass
 
-    def get_muebles(self):
-        return self.muebles
+    def get_nombre(self):
+        return self.nombre
 
+    def set_apellido(self,apellido):
+        self.apellido= apellido
+
+    def get_apellido(self):
+        return self.apellido
+
+class Cliente(Persona):
+    def __init__(self,nombre,apellido):
+        super().__init__(nombre,apellido)
+        self.id= 0
+
+    def set_nombre(self,nombre):
+        self.nombre= nombre
+
+    def set_id(self,id):
+        self.id= id
+
+    def get_id(self):
+        return self.id
+
+class Empleado(Persona):
+    def __init__(self,nombre,apellido):
+        super().__init__(nombre,apellido)
+        self.legajo= 0
+        self.comision= 0
+    
+    def set_nombre(self,nombre):
+        self.nombre= nombre
+
+    def set_legajo(self,legajo):
+        self.legajo= legajo
+
+    def get_legajo(self):
+        return self.legajo
+
+    def set_comision(self,comision):
+        self.comision= comision
+
+    def get_comision(self):
+        return self.comision
+        
+class Muebleria:
     def mostrar_menu_administracion(self):
         menu=int(input('''
 ------------------------------
-    VISTA ADMINISTRACIÓN
+     VISTA ADMINISTRACIÓN
 1. Cargar nuevo mueble
 2. Editar mueble
 3. Mostrar muebles registrados
@@ -237,8 +283,136 @@ Ingrese opción elegida: '''))
         else:
             return False
 
+class Venta:
+    def __init__(self):
+        self.id_producto= 0 #id del producto seleccionado
+        self.designacion= ""
+        self.cantidad= 0 #cantidad que va a comprar el cliente
+        self.stock= 0 #stock actual del producto_seleccionado
+        self.subtotal= 0 
+        self.total= 0
+        self.vendedor= ""
+
+    def set_id_producto(self,id_producto):
+        self.id_producto= id_producto
+
+    def get_id_producto(self):
+        return self.id_producto
+
+    def set_cantidad(self,cantidad):
+        self.cantidad= cantidad
+
+    def get_cantidad(self):
+        return self.cantidad
+    
+    def set_stock(self,stock):
+        self.stock= stock
+
+    def get_stock(self):
+        return self.stock
+
+    def set_subtotal(self,subtotal):
+        self.subtotal= subtotal
+
+    def get_subtotal(self):
+        return self.subtotal
+
+    def set_total(self,total):
+        self.total= total
+
+    def get_total(self):
+        return self.total
+
+    def set_vendedor(self,vendedor):
+        self.vendedor= vendedor
+
+    def get_vendedor(self):
+        return self.vendedor
+
+    def mostrar_muebles(self):
+        print("-----------------------------------------------\n              MUEBLES DISPONIBLES")
+        mi_cursor.execute("SELECT * FROM muebles ORDER BY designacion")
+        muebles= mi_cursor.fetchall()
+        for mueble in muebles:
+            if len(str(mueble[2])) > 3 and len(str(mueble[0])) > 1:
+                print("ID:",mueble[0],"| $"+str(mueble[2]), "| ",mueble[1])
+            elif len(str(mueble[2])) > 3:
+                print("ID:",mueble[0]," | $"+str(mueble[2]), "| ",mueble[1])
+            elif len(str(mueble[0])) > 1:
+                print("ID:",mueble[0],"| $"+str(mueble[2]), " | ",mueble[1])
+            else:
+                print("ID:",mueble[0]," | $"+str(mueble[2]), " | ",mueble[1])
+        print("---------------------------------------------")
+
+
+    '''
+    def elegir_mueble(self):
+        mueble_a_comprar= int(input("Ingrese el ID del mueble que desea comprar: "))
+        self.set_producto_seleccionado(mueble_a_comprar) #id
+    
+    def elegir_cantidad(self):
+        query= "SELECT designacion FROM muebles WHERE id = %s"
+        valor= self.get_id_producto()
+        mi_cursor.execute(query,(valor,))
+        mueble_elegido= mi_cursor.fetchone()
+        print("Mueble seleccionado: "+str(mueble_elegido).strip("',()").replace("_"," "))
+        cantidad= int(input("Ingrese la cantidad que desea comprar: "))
+        self.set_cantidad(cantidad)
+    '''
+
+    def elegir_mueble(self):
+        mueble_a_comprar= int(input("Ingrese el ID del mueble que desea comprar: ")) #o que el cliente desea comprar
+        self.id_producto(mueble_a_comprar)
+
+    def elegir_cantidad(self):
+        """Me muestra la designacion del mueble elegido, y me pide la cantidad que quiero comprar del mismo.
+        Ademas me trae el stock del producto solicitado."""
+        query1= "SELECT designacion FROM muebles WHERE id = %s"
+        id_mueble= self.get_id_producto()
+        mi_cursor.execute(query1,(id_mueble,))      
+        mueble_elegido= mi_cursor.fetchone()
+        print("Mueble seleccionado: "+str(mueble_elegido).strip("',()").replace("_"," "))
+
+        query2= "SELECT precio_unitario FROM muebles WHERE id = %s"
+        mi_cursor.execute(query2, (id_mueble,))
+        stock_mueble_elegido= mi_cursor.fetchone
+        self.set_stock(stock_mueble_elegido)
+
+        if stock_mueble_elegido > 0:
+            cantidad= int(input("Ingrese la cantidad que desea comprar: "))
+            self.set_cantidad(cantidad)
+        else:
+            print("No existe esa cantidad de",mueble_elegido,"en stock actualmente.")
+
+    def calcular_precio(self):
+        pass
+
+    def descontar_stock(self):
+        """Descuenta el atributo 'cantidad' del stock del producto cuyo id se ingreso en el atributo 'producto_seleccionado'."""
+        pass
+
+    def procesar_compra(self):
+        self.elegir_cantidad()
+        self.descontar_stock()
+
+
+    def prueba(self):
+        print(self.get_producto_seleccionado())
+        print(self.get_cantidad())
+        print(self.get_subtotal())
+        print(self.get_total())
+        print(self.get_vendedor())
+      
+
+    
+
+    def comprar_muebles(self): #PRUEBA
+        self.mostrar_muebles()
+        self.elegir_mueble()
+        self.elegir_cantidad()
 #main
 muebleriaPOO= Muebleria()
+venta0000= Venta()
 print('¡Bienvenido a Muebleria POO!')
 menu= int(input('-------------------\nVistas:\n1. Administración\n2. Ventas\n3. Gerencia\n-------------------\nIngrese el número de vista que desea consultar: '))
 if menu == 1:
@@ -256,8 +430,23 @@ if menu == 1:
         if opc == 5:
             muebleriaPOO.eliminar_mueble()
         on= muebleriaPOO.realizar_otra_operacion()
+
 if menu == 2:
-    print("Vista Ventas")
+    on= True
+    while on == True:
+        print("Vista Ventas")
+        venta0000.comprar_muebles()
+        venta0000.prueba()
+        on= muebleriaPOO.realizar_otra_operacion()
+
 if menu == 3:
     print("Vista Gerencia")
 print("¡Gracias por utilizar el software!")
+
+'''
+Vista Vendedor:
+Desarrollaremos el módulo venta:
+Ofreceremos todos los muebles cargados en la vista administración y los clientes podrán comprar los muebles que quieran, cuantas veces quieran.
+Nos piden generar un ticket para cada cliente indicando:
+El nombre del producto seleccionado, las cantidades compradas, el subtotal por producto, el nombre del vendedor y el total por producto.
+'''
